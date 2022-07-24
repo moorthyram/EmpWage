21e3209097b314bdb2a719dcc0c403bab021e574
Ramamoorthy <ramamoorthy300798@gmail.com>
2022-07-24 23:44:03 +0530

    UC1-Check Emp Presence

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EmpWageFullProgram
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constants
+            int Emp_Full_TimePresent = 1;
+
+            Random random = new Random();
+
+            // Computation
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == Emp_Full_TimePresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
