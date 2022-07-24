ad177bd959d65fa743d1b1b0274fd831d7d2935f
Ramamoorthy <ramamoorthy300798@gmail.com>
2022-07-25 00:29:42 +0530

    UC6-Calc-NumOfWorkDays-or-TWHPMonth

--- Program.cs
+++ Program.cs
@@ -7,17 +7,19 @@
         public const int Emp_Part_TimePresent = 2;
         public const int Emp_Rate_per_Hour = 20;
         public const int Num_Of_Working_Days = 2;
+        public const int Max_Hrs_In_Month = 10;
         static void Main(string[] args)
         {
 
             // Variables
             int empWage = 0;
             int empHrs = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             // Computation
-            for (int day = 0; day < Num_Of_Working_Days; day++)
+            while(totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days)
             {
-
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
 
@@ -33,10 +35,10 @@
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * Emp_Rate_per_Hour;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * Emp_Rate_per_Hour;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
